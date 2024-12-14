c2598f2a8f8a4af3b970800d4b5acf832f1f93ae
Antoni Dąbrowski <rtantak01@gmail.com>
2024-12-14 18:44:52 +0100

    add basic error message functionality

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
-const event01 = document.getElementById("event-01");
+const containerSlot = document.querySelector(".notif-slot")
+const errorButton = document.getElementById("event-01");
 
-event01.addEventListener("click", () => {
-  console.log("button clicked")
-})
+var notif = "";
+
+errorButton.addEventListener("click", addNotif())
+
+function addNotif() {
+  containerSlot.innerHTML += '<div class="notif danger"> <h3>Error!</h3> <br> <p>Contents of the error</p> </div> ';
+notif = document.querySelector('.notif');
+
+return notif;
+};
+
+let notifState = "vanish";
+
+if (notifState == "vanish" && notif != null) { setTimeout(() => {
+  notif.style.animation = "vanishing 1000ms ease forwards"
+}, 3000) }
+
+if (notifState == "keepDisplaying") {
+  notif.style.animation = ""
+}
+
