b6d856782f70b265505da43bd37b4f62507a23e8
Antoni Dąbrowski <rtantak01@gmail.com>
2024-12-15 13:05:55 +0100

    create an Object-based mechanism for notifications

--- js/index.js
+++ js/index.js
@@ -1,24 +1,44 @@
 const containerSlot = document.querySelector(".notif-slot")
 const errorButton = document.getElementById("event-01");
+let notifs = []
 
-var notif = "";
+var notifNumber = 0
 
-errorButton.addEventListener("click", addNotif())
-
-function addNotif() {
-  containerSlot.innerHTML += '<div class="notif danger"> <h3>Error!</h3> <br> <p>Contents of the error</p> </div> ';
-notif = document.querySelector('.notif');
-
-return notif;
-};
-
-let notifState = "vanish";
-
-if (notifState == "vanish" && notif != null) { setTimeout(() => {
-  notif.style.animation = "vanishing 1000ms ease forwards"
-}, 3000) }
-
-if (notifState == "keepDisplaying") {
-  notif.style.animation = ""
+function Notification(type, messageHead, message, notifId) {
+  this.type = type;
+  this.notifId = notifId;
+  this.messageHead = messageHead;
+  this.message = message;
 }
 
+Notification.prototype.add = function() {
+  this.notifId = notifNumber
+  containerSlot.innerHTML += `
+    <div 
+      id="notif${this.notifId}" 
+      class="notif danger"> 
+      <h3>${this.messageHead}</h3> 
+      <br> 
+      <p>${this.message}</p> 
+      <br>
+      <button class="ui dismiss">dismiss</button>
+    </div> 
+  `
+  this.domElement = document.getElementById(`notif${this.notifId}`)
+  this.dismissButton = document.querySelector(`#notif${this.notifId} .dismiss`)
+  
+  const fadeOut = new Promise((resolve, reject) => {
+    this.dismissButton.addEventListener("click", () => {
+      this.domElement.style.animation = "vanishing 1000ms ease forwards"
+      resolve("notif vanished")
+    })
+  })
+  fadeOut.then(value => {
+    setTimeout( () => { this.domElement.remove() }, 1000)
+  })
+
+  notifNumber += 1
+}
+errorButton.addEventListener("click", () => {
+  notifs.push(new Notification('danger', 'Test message', 'test message text', notifNumber).add())
+})
