90b8a2959948240c4565b2d8da0f1d2b6b85e51a
Antoni Dąbrowski <rtantak01@gmail.com>
2025-01-11 22:16:14 +0100

    divide the css file into many subsections to easily navigate the project

--- js/shadowComponent.js
+++ js/shadowComponent.js
@@ -1,8 +1,38 @@
+const template = document.createElement("template")
+template.innerHTML = `
+<style>
+
+*:where(.ui) {
+  --shade-color: var(--accent-color);
+  background: none;
+  border: 1px solid black;
+  box-sizing: border-box;
+  border-radius: var(--border-r);
+  padding: 1rem 1.4em;
+  text-decoration: none;
+  padding: var(--btn-padding-h) var(--btn-padding-v);
+  background-color: var(--white); 
+  color: var(--text-secondary);
+  border: none;
+  box-shadow: 0px .3em 0px var(--shade-color),
+              0px .2em 16px var(--non-bg);
+  width: fit-content;
+  margin-bottom: 3em; 
+
+  justify-self: center;
+}
+</style>
+<button class="ui"><slot></slot></button>
+`
+
+
 class uiButton extends HTMLElement {
   constructor() {
     super()
-    this.buttonTheme = 'danger';
-    this.innerHTML = `<button class="ui ${this.buttonTheme}">${this.innerText}</button>`
+    const shadow = this.attachShadow({mode: "open"})
+    shadow.append(template.content.cloneNode('true'))
+    // template.buttonTheme = 'danger';
+    // template.innerHTML = `<button class="ui ${this.buttonTheme}">${this.innerText}</button>`
   }
 }
 
